d9837de7cca5518797f60a8a977dcd379d38e7b7
AnnieisOK <48488054+AnnieisOK@users.noreply.github.com>
2020-01-31 22:52:19 +0200

    Delete footer.js

--- AnnieIsOk/ProiectFinal/scripts/footer.js
+++
@@ -1,6 +0,0 @@
-"use strict"
-function addYear () {
-    var companyInfo = document.getElementById('companyInfo');
-    companyInfo.appendChild(document.createTextNode(new Date().getFullYear()));
-}
-addYear ();
